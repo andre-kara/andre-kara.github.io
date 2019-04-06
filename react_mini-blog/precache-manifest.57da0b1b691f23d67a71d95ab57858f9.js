64577d5c9cf607a2b250d6519f6b640bef726ac9
Andrei Karagayr <andreikara404@gmail.com>
2019-04-06 16:26:01 +0300

    app mini blog

---
+++ react_mini-blog/precache-manifest.57da0b1b691f23d67a71d95ab57858f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a30a501fb5c519c0d83f",
+    "url": "/static/js/main.a30a501f.chunk.js"
+  },
+  {
+    "revision": "c9d2e6c820abbeb81237",
+    "url": "/static/js/2.c9d2e6c8.chunk.js"
+  },
+  {
+    "revision": "a30a501fb5c519c0d83f",
+    "url": "/static/css/main.eda00432.chunk.css"
+  },
+  {
+    "revision": "c9d2e6c820abbeb81237",
+    "url": "/static/css/2.dd02456e.chunk.css"
+  },
+  {
+    "revision": "36a2caa45bdce86095131e95d8fcaf96",
+    "url": "/index.html"
+  }
+];
